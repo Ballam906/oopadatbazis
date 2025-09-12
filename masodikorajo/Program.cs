24872ef6c8a9bc2e08c494769a314ec755207ce5
ballam88 <ballam@kkszki.hu>
2025-09-12 10:43:55 +0200

    Base

---
+++ masodikorajo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace masodikorajo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
